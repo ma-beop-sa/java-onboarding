81403165f272033421b654d481e5a74d4ba6158d
hyunjungg <hj351016@gmail.com>
2022-11-01 02:51:18 +0900

    feat : 숫자의 각 자릿수를 구하는 기능 개발
    3,6,9 포함 여부 확인을 위해 각 자릿수를 나누는 로직을 개발한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+     
+        for(int i = 1; i <= number; i++) {
+        	answer += getClaps(i);
+        }
+        
         return answer;
     }
+    
+    
+    private ArrayList<Integer> calcDigit(int number){
+		
+		ArrayList<Integer> digitsArray = new ArrayList<Integer>();
+		while (number > 0) {
+			digitsArray.add(number % 10);
+			number /= 10;
+		}
+		
+		return digitsArray;
+	}
+    
 }
