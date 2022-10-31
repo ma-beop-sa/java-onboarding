7dd675cc1abb87a9671bf3f5d37754d819b7d400
hyunjungg <hj351016@gmail.com>
2022-11-01 03:04:32 +0900

    feat : 알파벳을 역순으로 매칭한 후 변환하는 기능
    요구사항의 내용대로 주어진 문자열 중 알파벳인 부분을 확인하고 변환하는 기능을 개ㅂ발

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        String result = "";
+        
+        for(int i = 0 ; i < word.length(); i++) {
+        	char ch = word.charAt(i);
+        	
+    		if (true == Character.isLowerCase(ch)) {
+				
+    			result += (char)(('z' - ch) + 'a');
+				
+			}else if(true == Character.isUpperCase(ch)) {
+				
+				result += (char)(('Z' - ch) + 'A');
+        }
+    		
+        answer = result;
+    		
         return answer;
     }
 }
