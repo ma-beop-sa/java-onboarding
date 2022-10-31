bff1a97d1d1b9e28da5298f626ad1cb5c962081e
hyunjungg <hj351016@gmail.com>
2022-11-01 02:36:52 +0900

    feat : 중복 문자열 확인 기능 개발
    중복되는 문자열 체크 로직 개발

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,5 +10,7 @@ public class Problem2 {
     	Pattern pattern = Pattern.compile("(\\w)\\1+");
     	Matcher regex = pattern.matcher(cryptogram);
         return matcher.find();
+        
+        
     }
 }
