f301a2c3edace9108fdbf6d0ccf3209d9c1fda55
hyunjungg <hj351016@gmail.com>
2022-11-01 02:30:25 +0900

    feat : 문자열 중복 확인 기능 추가
    중복되는 문자열 유무를 체크하는 로직 개발

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,10 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+    
+    private boolan checkDuplicate(String cryptogram) {
+    	Pattern pattern = Pattern.compile("(\\w)\\1+");
+    	Matcher regex = pattern.matcher(cryptogram);
+        return matcher.find();
+    }
 }
